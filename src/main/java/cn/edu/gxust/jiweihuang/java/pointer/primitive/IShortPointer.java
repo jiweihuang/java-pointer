78298eac99b36a9706af4b983771927d8bc0a640
JiweiHuang <huangjiwei@gxust.edu.cn>
2019-12-26 13:36:59 +0800

    update projects 2019-12-25

--- src/main/java/cn/edu/gxust/jiweihuang/java/pointer/primitive/IShortPointer.java
+++ src/main/java/cn/edu/gxust/jiweihuang/java/pointer/primitive/IShortPointer.java
@@ -39,16 +39,24 @@ package cn.edu.gxust.jiweihuang.java.pointer.primitive;
 import cn.edu.gxust.jiweihuang.java.pointer.IArrayPointer;
 
 /**
- * 接口 {@code IShortPointer} 用于表征一个指向{@code short} 型数据区域的指针。
+ * 接口{@code IShortPointer}用于表征一个指向{@code short}型数组的指针。<p>
+ * 注意：方法 {@code move}、{@code copy}和{@code reset}的差异：<p>
+ * (1)方法{@code move}是移动指针的指向，假如一个指针当前的指向索引为2，
+ * 向右移动1个索引，则其新指向为3.<p>
+ * (2)方法 {@code copy}是拷贝指针，即通过拷贝创建了一个新的指针，
+ * 拷贝的指针的指向与原指针的指向相同，
+ * 但之后该指针的指向移动将与原指针无关。<p>
+ * (3)方法 {@code reset}是只是重置指针，并不创建新指针，
+ * 重置是指将指针的指向设置为0。<p>
+ * 上述三种方法可以组合起来使用，以达到创建、修改和重置等目的。
  * <p>
- * Development status: Release    # Developing <p>
- * Completion date: 20191022 <p>
- * Test status: None    # Missing, Finished <p>
- * Doc status: Finished    # Missing <p>
+ * Development status：Finished     # Developing, Finished  <p>
+ * Javadoc status: Finished         # Missing, Developing, Finished  <p>
+ * Test status: None                # None, Missing, Developing, Finished  <p>
+ * Last revision date: 2019-12-25 <p>
  *
  * @author JiweiHuang
- * @see IArrayPointer
- * @since 20191020
+ * @since 20191205
  */
 public interface IShortPointer extends IArrayPointer {
     /**
@@ -60,8 +68,8 @@ public interface IShortPointer extends IArrayPointer {
     short get(int index);
 
     /**
-     * 获取指针指向处的数据。
-     * 注意：如果指针指向数据区域外，则将抛出异常。
+     * 获取指针当前指向处的数据。
+     * 注意：如果指针当前指向数组索引范围之外，则将抛出异常。
      *
      * @return 指针指向处的数据。
      */
@@ -78,12 +86,57 @@ public interface IShortPointer extends IArrayPointer {
     void set(int index, short value);
 
     /**
-     * 设置指针指向处的数据。
-     * 注意：如果指针指向数据区域外，则将抛出异常。
+     * 设置指针当前指向处的数据。
+     * 注意：如果指针当前指向数组索引范围之外，则将抛出异常。
      *
      * @param value 需要设置的值。
      */
     default void set(short value) {
         set(0, value);
+    }
+    /**
+     * 移动指针的指向后返回该指针，
+     * 内部实现为 {@code point = getPoint() + offset}。
+     *
+     * @param offset 指针指向的移动量。
+     * @return 指针指向移动后的指针。
+     */
+    IShortPointer move(int offset);
+
+    /**
+     * 通过拷贝的方式创建新指针，拷贝后的指向与原指针的指向相同，
+     * 但两者随后的移动将彼此分离，互补相关。
+     *
+     * @return 拷贝后得到的新指针。
+     */
+    IShortPointer copy();
+
+    /**
+     * 通过拷贝的方式创建新指针，并移动该指针指向新的索引。
+     *
+     * @param offset 指针指向的偏移量。
+     * @return 拷贝并移动后的新指针。
+     */
+    default IShortPointer copy(int offset) {
+        return copy().move(offset);
+    }
+
+    /**
+     * 将指针的指向重置为{@code 0}。
+     *
+     * @return 指针指向重置后的指针。
+     */
+    IShortPointer reset();
+
+    /**
+     * 将指针的指向重置为{@code offset}。<p>
+     * 内部实现为先将指针指向重置为{@code 0}，
+     * 然后移动至 {@code offset}。
+     *
+     * @param offset 重置后指针指向。
+     * @return 指针指向重置至指定索引后的指针。
+     */
+    default IShortPointer reset(int offset) {
+        return reset().move(offset);
     }
 }
