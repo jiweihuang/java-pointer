602ba7e606105a5e34cc07ea8aeed5db2d095404
JiweiHuang <huangjiwei@gxust.edu.cn>
2019-12-26 22:10:32 +0800

    update projects 2019-12-25

--- src/main/java/cn/edu/gxust/jiweihuang/java/pointer/cminpack/JavaMinpack.java
+++ src/main/java/cn/edu/gxust/jiweihuang/java/pointer/cminpack/JavaMinpack.java
@@ -1,32 +1,37 @@
 /*
- *                             Apache License
- *                        Version 2.0, January 2004
- *                     http://www.apache.org/licenses/
+ *                           MIT License
  *
  * Copyright (c) 2019-2020, Jiwei Huang. All Rights Reserved.
  *
- * This file is part of JavaMinpack (https://github.com/gxusthjw/JavaMinpack).
+ * This file is a part of projects for java-pointer
+ *  (https://github.com/jiweihuang/java-pointer)
  *
- *  ------------------------- Contact Author ----------------------------------
- *  Author: Jiwei Huang.
+ *  -------------------------Contact Author--------------------------------
+ *  Author: Jiwei Huang
+ *  E-mail: jiweihuang@yeah.net, huangjiwei@gxust.edu.cn
  *  Organization: Guangxi University of Science and Technology
  *  Postcode：545006
- *  Contact number：0772-2687033(School Office)  0772-2687033（Fax）
+ *  Contact number：0772-2687033(School Office), 0772-2687033（Fax）
  *  Address：#268 Avenue Donghuan, Chengzhong District, Liuzhou, Guangxi, China
- *  ---------------------------------------------------------------------------
+ *  -----------------------------------------------------------------------
  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  */
 package cn.edu.gxust.jiweihuang.java.pointer.cminpack;
